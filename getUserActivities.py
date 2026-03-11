b106dccd8b7d0620c70ca22ff94c8681096f7067
Nico <vargwen@proton.me>
2026-03-11 15:42:32 +0100

    Script permettant de récupérer les 5 derniers run avec l'athlete_id

---
+++ getUserActivities.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+def get_activities(access_token, limit = 5):
+    """Récupère les 5 dernières activités."""
+    url = "https://www.strava.com/api/v3/athlete/activities"
+    headers = {'Authorization': f'Bearer {access_token}'}
+    params = {'per_page': 20}
+
+    response = requests.get(url, headers=headers, params=params).json()
+    running_activities = [act for act in response if act['type'] == 'Run']
+
+    return running_activities[:limit]
